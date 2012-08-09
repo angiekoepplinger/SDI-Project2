c5117937e08af34be221dee18cfc5132335a654b
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-08-09 07:08:52 -0500

    Array function
    Created array function to show how much of each topping I will add to
    my pizza

--- Koepplinger_Angela_assignment2.js
+++ Koepplinger_Angela_assignment2.js
@@ -1,2 +1,25 @@
-alert("JavaScript works!");
-test this out, add text
+
+//Angela Koepplinger
+//SDI: 1208
+//August 09, 2012
+//Project 2
+
+
+var toppings= ["cheese", "pepperoni", "olives", "peppers", "artichokes"],
+	cupsOfToppings= [ 1, .5, .25, .25, .25];
+
+var addToppings= function (numberPizzas, ingredients) {
+	for (toppingsNumber = 0; toppingsNumber < toppings.length; toppingsNumber++){
+		var whichTopping= ingredients[toppingsNumber]
+		var measureToppings= cupsOfToppings[toppingsNumber]
+		var toppingsTotal = numberPizzas * cupsOfToppings[toppingsNumber];
+		console.log("I will add " + toppingsTotal + " cups of " + whichTopping + " to my pizza!");
+	}
+
+	return ingredients;
+};
+
+//for (toppingsNumber = 0; toppingsNumber < toppings.length; toppingsNumber++){
+		addToppings(3, toppings);
+//}
+
