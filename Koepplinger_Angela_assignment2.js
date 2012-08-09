79a1f40cbad4344ab2a518cc2af418736af3cfe7
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-08-09 15:57:32 -0500

    Completed Procedure, Boolean & Array
    These are finished, and correct.  Moving on to number and string.

--- Koepplinger_Angela_assignment2.js
+++ Koepplinger_Angela_assignment2.js
@@ -7,8 +7,7 @@
 
 
 //Boolean
-
-var qtyPizzas = 3
+var qtyPizzas = 2
 var starving = true
 var numberPizzas = function(qty, veryHungry) {
 	var hunger=true;
@@ -17,32 +16,28 @@ var numberPizzas = function(qty, veryHungry) {
 	} else {
 		console.log("I am not overly hungry, I think I'll just make 1 pizza for now");
 	}
+	return starving;
 }
 var howManyPizzas = numberPizzas(qtyPizzas, starving)
 
 
 
 
-
-
-
-
-/*
 //Procedure
 var cookTemp= 375;
 var pizzaTemp= function(setTemp) {
 	var ovenTemp=375;
 	if (ovenTemp === setTemp){
-		console.log("Our oven has finished preheating, let's make a pizza!");
+		console.log("Our oven has finished preheating, let's start adding our toppings!");
 	} else {
 		console.log("While our oven finishes preheating, let's work on adding our toppings to our pizza");
 	};	
 }
 var preheatOven=pizzaTemp(cookTemp)
-*/
 
 
-/*
+
+
 //Array
 var toppings= ["cheese", "pepperoni", "olives", "peppers", "artichokes"],
 	cupsOfToppings= [ 1, .5, .25, .25, .25];
@@ -52,7 +47,7 @@ var addToppings= function (numberPizzas, ingredients) {
 		var whichTopping= ingredients[toppingsNumber]
 		var measureToppings= cupsOfToppings[toppingsNumber]
 		var toppingsTotal = numberPizzas * cupsOfToppings[toppingsNumber];
-		console.log("I will add " + toppingsTotal + " cups of " + whichTopping + " to my pizza!");
+		console.log("I will add " + toppingsTotal + " cups of " + whichTopping + " to my pizzas!  " + toppingsTotal/numberPizzas  + " cups to each.");
 	}
 
 	return ingredients;
@@ -61,4 +56,4 @@ var addToppings= function (numberPizzas, ingredients) {
 for (toppingsNumber = 0; toppingsNumber < toppings.length; toppingsNumber++){
 		addToppings(qtyPizzas, toppings);
 }
-*/
+
