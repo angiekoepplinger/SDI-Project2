835ca007cb0f6acea1ba1a465af85df9dc05f857
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-08-09 11:36:03 -0500

    Begin Procedure

--- Koepplinger_Angela_assignment2.js
+++ Koepplinger_Angela_assignment2.js
@@ -5,6 +5,15 @@
 //Project 2
 
 
+//Procedure
+var cookTemp= 375;
+
+var pizzaTemp= function(setTemp) {
+	var ovenTemp=375;
+	if (ovenTemp===setTemp)
+
+
+}
 var toppings= ["cheese", "pepperoni", "olives", "peppers", "artichokes"],
 	cupsOfToppings= [ 1, .5, .25, .25, .25];
 
